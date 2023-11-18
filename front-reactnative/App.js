9ab87a37ca044ab71fcf1f9d2050775468329ad9
Fabio <fabioborgesouzajunior@gmail.com>
2023-11-18 14:53:49 -0300

    testing expo app at phone

--- front-reactnative/App.js
+++ front-reactnative/App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Rodando nossa aplicação no celular!</Text>
+      <Text>Essa aplicação vai ser top Dutrinha, bjs bjs</Text>
+      <Text>Que papai do céu ilumine seu bracinho </Text>
+      <Text>direito e faça ficar do mesmo tamanho do esquerdo</Text>
+      <Image source={require("./assets/strong.png")}/>
       <StatusBar style="auto" />
     </View>
   );
