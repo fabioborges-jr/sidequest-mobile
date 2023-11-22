bee8ab37cb3260beacf5a43a2f5fe5a9fe474fa8
Fabio <fabioborgesouzajunior@gmail.com>
2023-11-22 19:29:19 -0300

    Logo added at auth screen

--- front-reactnative/App.js
+++ front-reactnative/App.js
@@ -1,14 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Rodando nossa aplicação no celular!</Text>
-      <Text>Essa aplicação vai ser top Dutrinha, bjs bjs</Text>
-      <Text>Que papai do céu ilumine seu bracinho </Text>
-      <Text>direito e faça ficar do mesmo tamanho do esquerdo</Text>
-      <Image source={require("./assets/strong.png")}/>
+      <Image source={require("./assets/logo.png")}/>
+      <Text>SIDE QUEST</Text>
       <StatusBar style="auto" />
     </View>
   );
